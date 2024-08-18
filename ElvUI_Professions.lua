50425e1cf4482d499920796820c3b6df6df80e83
fuba <q3fuba@gmail.com>
2024-08-18 02:59:44 +0200

    Update for TWW

--- ElvUI_Professions.lua
+++ ElvUI_Professions.lua
@@ -17,29 +17,31 @@ local displayString = ""
 local tooltipString = ""
 local TradeSkillUIOpened = false
 
+local STAT_CATEGORY_FUBA = "fuba"
+
 -- Main Profession localisations
-local sAlchemy = GetSpellInfo(2259)
-local sBlacksmithing = GetSpellInfo(2018)
-local sEnchanting = GetSpellInfo(7411)
-local sEngineering = GetSpellInfo(4036)
-local sHerbalism = GetSpellInfo(13614)
-local sLeatherworking = GetSpellInfo(2108)
-local sLockpicking = GetSpellInfo(1809)
-local sMining = GetSpellInfo(2575)
-local sSkinning = GetSpellInfo(8613)
-local sSmelting = GetSpellInfo(2656)
-local sTailoring = GetSpellInfo(3908)
-local sJewelcrafting = GetSpellInfo(25229)
+local sAlchemy 					= C_Spell and C_Spell.GetSpellName(2259) or GetSpellInfo(2259)
+local sBlacksmithing 		= C_Spell and C_Spell.GetSpellName(2018) or GetSpellInfo(2018)
+local sEnchanting 			= C_Spell and C_Spell.GetSpellName(7411) or GetSpellInfo(7411)
+local sEngineering 			= C_Spell and C_Spell.GetSpellName(4036) or GetSpellInfo(4036)
+local sHerbalism 				= C_Spell and C_Spell.GetSpellName(13614) or GetSpellInfo(13614)
+local sLeatherworking 	= C_Spell and C_Spell.GetSpellName(2108) or GetSpellInfo(2108)
+local sLockpicking 			= C_Spell and C_Spell.GetSpellName(1809) or GetSpellInfo(1809)
+local sMining 					= C_Spell and C_Spell.GetSpellName(2575) or GetSpellInfo(2575)
+local sSkinning 				= C_Spell and C_Spell.GetSpellName(8613) or GetSpellInfo(8613)
+local sSmelting 				= C_Spell and C_Spell.GetSpellName(2656) or GetSpellInfo(2656)
+local sTailoring 				= C_Spell and C_Spell.GetSpellName(3908) or GetSpellInfo(3908)
+local sJewelcrafting 		= C_Spell and C_Spell.GetSpellName(25229) or GetSpellInfo(25229)
 
 -- Second Profession localisations
-local sCooking = GetSpellInfo(2550)
-local sFirstAid = GetSpellInfo(3273)
-local sFishing = GetSpellInfo(7620)
-local sArchaeology = GetSpellInfo(78670)
+local sCooking 					= C_Spell and C_Spell.GetSpellName(2550) or GetSpellInfo(2550)
+local sFirstAid 				= C_Spell and C_Spell.GetSpellName(3273) or GetSpellInfo(3273)
+local sFishing 					= C_Spell and C_Spell.GetSpellName(7620) or GetSpellInfo(7620)
+local sArchaeology 			= C_Spell and C_Spell.GetSpellName(78670) or GetSpellInfo(78670)
 
 -- Profession Tracking localisations
-local sFindHerbs = GetSpellInfo(2383)
-local sFindMinerals = GetSpellInfo(2580)
+local sFindHerbs 				= C_Spell and C_Spell.GetSpellName(2383) or GetSpellInfo(2383)
+local sFindMinerals 		= C_Spell and C_Spell.GetSpellName(2580) or GetSpellInfo(2580)
 
 local mainprof1 = {sAlchemy, sBlacksmithing, sEnchanting, sEngineering, sJewelcrafting, sLeatherworking, sTailoring, sMining}
 local mainprof2 = {sHerbalism, sSkinning}
@@ -47,7 +49,7 @@ local secprof = {sCooking, sFirstAid, sFishing, sArchaeology}
 
 local function GetProfessionName(index)
   local name = GetProfessionInfo(index)
-  return name
+  return name or _G.UNKNOWN or ""
 end
 
 local function OnEvent(self, event, ...)
@@ -112,28 +114,31 @@ local function OnEvent(self, event, ...)
 end
 
 local function Click(self, button)
-
+	--SPELL_FAILED_NOT_KNOWN
+	--ADVENTURE_TRACKING_OPEN_PROFESSION_ERROR_TEXT
   local prof1, prof2, archy, fishing, cooking, firstAid = GetProfessions()
   local profname
 	if (button == "LeftButton") or (button == "RightButton") then
 		if (button == "LeftButton") then
-			if IsAltKeyDown() and cooking ~= nil then
+			if IsShiftKeyDown() and cooking ~= nil then
 				profname = sCooking
+			elseif IsControlKeyDown() then
+				profname = sArchaeology
 			else
 				if E.private.profdt.prof == "prof1" then
-					profname = prof1 and GetProfessionInfo(prof1) or "";
+					profname = prof1 and GetProfessionInfo(prof1) or ADVENTURE_TRACKING_OPEN_PROFESSION_ERROR_TEXT or "";
 				else
-					profname = prof2 and GetProfessionInfo(prof2) or "";
+					profname = prof2 and GetProfessionInfo(prof2) or ADVENTURE_TRACKING_OPEN_PROFESSION_ERROR_TEXT or "";
 				end
 			end
 		elseif (button == "RightButton") then
-			if IsAltKeyDown() and fishing ~= nil then
+			if IsShiftKeyDown() and fishing ~= nil then
 				profname = sFishing
 			else
 				if E.private.profdt.prof == "prof1" then
-					profname = prof2 and GetProfessionInfo(prof2) or "";
+					profname = prof2 and GetProfessionInfo(prof2) or ADVENTURE_TRACKING_OPEN_PROFESSION_ERROR_TEXT or "";
 				else
-					profname = prof1 and GetProfessionInfo(prof1) or "";
+					profname = prof1 and GetProfessionInfo(prof1) or ADVENTURE_TRACKING_OPEN_PROFESSION_ERROR_TEXT or "";
 				end
 			end
 		end
@@ -229,30 +234,43 @@ local function OnEnter(self)
   if E.private.profdt.hint then
 		local prof1, prof2, archy, fishing, cooking, firstAid = GetProfessions()
 		if E.private.profdt.prof == "prof1" then			
-			profname1 = prof1 and GetProfessionInfo(prof1) or "";
-			profname2 = prof2 and GetProfessionInfo(prof2) or "";
+			profname1 = prof1 and GetProfessionInfo(prof1) or ADVENTURE_TRACKING_OPEN_PROFESSION_ERROR_TEXT or "";
+			profname2 = prof2 and GetProfessionInfo(prof2) or ADVENTURE_TRACKING_OPEN_PROFESSION_ERROR_TEXT or "";
 		else
-			profname1 = prof2 and GetProfessionInfo(prof2) or "";
-			profname2 = prof1 and GetProfessionInfo(prof1) or "";
+			profname1 = prof2 and GetProfessionInfo(prof2) or ADVENTURE_TRACKING_OPEN_PROFESSION_ERROR_TEXT or "";
+			profname2 = prof1 and GetProfessionInfo(prof1) or ADVENTURE_TRACKING_OPEN_PROFESSION_ERROR_TEXT or "";
 		end
-	
+		
+		--SHIFT_KEY = "ALT key";
+		--SHIFT_KEY_TEXT = "ALT";
+		--ALT_KEY = "ALT key";
+		--ALT_KEY_TEXT = "ALT";
+		--CTRL_KEY = "CTRL key";
+		--CTRL_KEY_TEXT = "CTRL";
+		--LCTRL_KEY_TEXT = "Left CTRL";
+		--RCTRL_KEY_TEXT = "Right CTRL";
+		--LCMD_KEY_TEXT = "Left CMD";
+		--RCMD_KEY_TEXT = "Right CMD";
+		
     DT.tooltip:AddLine(" ")
     DT.tooltip:AddDoubleLine(KEY_BUTTON1, profname1, 1, 1, 1, 1, 1, 0)
     DT.tooltip:AddDoubleLine(KEY_BUTTON2, profname2, 1, 1, 1, 1, 1, 0)
-    DT.tooltip:AddDoubleLine(ALT_KEY.." + "..KEY_BUTTON1, sCooking, 1, 1, 1, 1, 1, 0)
-		DT.tooltip:AddDoubleLine(ALT_KEY.." + "..KEY_BUTTON2, sArchaeology, 1, 1, 1, 1, 1, 0)
+    if cooking then DT.tooltip:AddDoubleLine(SHIFT_KEY.." + "..KEY_BUTTON1, sCooking, 1, 1, 1, 1, 1, 0) end
+		if fishing then DT.tooltip:AddDoubleLine(SHIFT_KEY.." + "..KEY_BUTTON2, sFishing, 1, 1, 1, 1, 1, 0) end
+		if archy then DT.tooltip:AddDoubleLine(CTRL_KEY.." + "..KEY_BUTTON1, sArchaeology, 1, 1, 1, 1, 1, 0) end
 		DT.tooltip:AddDoubleLine(KEY_BUTTON3, _G["SPELLBOOK"], 1, 1, 1, 1, 1, 0)
   end
 
   DT.tooltip:Show()
 end
 
-local function ValueColorUpdate(hex, r, g, b)
+local function ValueColorUpdate(self, hex, r, g, b)
   displayString = join("", "|cffffffff%s:|r ", hex, "%d|r/", hex, "%d|r")
   tooltipString = join("" , hex, "%d|r|cffffffff/|r", hex, "%d|r")
   if lastPanel ~= nil then OnEvent(lastPanel) end
+	--OnEvent(self)
 end
-E["valueColorUpdateFuncs"][ValueColorUpdate] = true
+--E["valueColorUpdateFuncs"][ValueColorUpdate] = true
 
 V["profdt"] = {
   ["prof"] = "prof1",
@@ -309,5 +327,21 @@ local function InjectOptions()
   }
 end
 
+--[[
+	DT:RegisterDatatext(name, category, events, eventFunc, updateFunc, clickFunc, onEnterFunc, onLeaveFunc, localizedName, objectEvent, colorUpdate)
+
+	name - name of the datatext (required) [string]
+	category - name of the category the datatext belongs to. [string]
+	events - must be a table with string values of event names to register [string or table]
+	eventFunc - function that gets fired when an event gets triggered [function]
+	updateFunc - onUpdate script target function [function]
+	click - function to fire when clicking the datatext [function]
+	onEnterFunc - function to fire OnEnter [function]
+	onLeaveFunc - function to fire OnLeave, if not provided one will be set for you that hides the tooltip. [function]
+	localizedName - localized name of the datetext [string]
+	objectEvent - register events on an object, using E.RegisterEventForObject instead of panel.RegisterEvent [function]
+	colorUpdate - function that fires when called from the config when you change the dt options. [function]
+]]
+
 EP:RegisterPlugin(..., InjectOptions)
-DT:RegisterDatatext("Professions", nil, {"PLAYER_ENTERING_WORLD", "CHAT_MSG_SKILL", "TRADE_SKILL_LIST_UPDATE", "TRADE_SKILL_DATA_SOURCE_CHANGED"}, OnEvent, nil, Click, OnEnter)
+DT:RegisterDatatext("Professions", STAT_CATEGORY_FUBA, {"PLAYER_ENTERING_WORLD", "CHAT_MSG_SKILL", "TRADE_SKILL_LIST_UPDATE", "TRADE_SKILL_DATA_SOURCE_CHANGED"}, OnEvent, nil, Click, OnEnter, nil, nil, nil, ValueColorUpdate)
